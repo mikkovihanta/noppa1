e5e969c33e18e5e7a1c9dfd495069e94a210a15b
mikkovihanta <mikko.vihanta@gmail.com>
2024-09-13 13:49:14 +0300

    scripti toimii

--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,9 @@
+const noppis = document.getElementById('dice');
+const arpakuutio = document.getElementById('noppa');
 
+        function heita() {
+            const random = Math.floor(Math.random() * 6) + 1;
+            arpakuutio.src = `./img/${random}.png`; 
+        }
+        noppis.addEventListener('click', heita);
     
